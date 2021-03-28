dfde2414a8425ca8a1e069ca1ac84e85fe08ff5e
Ramiro Di Carlo <ramirodicarlo1@gmail.com>
2021-03-28 19:03:21 -0300

    Finish Var and Operators

---
+++ Var-and-Operators.js
@@ -0,0 +1,24 @@
+console.log('-- Executing Var-and-Operators.js --');
+//////////////////// 1-a //////////////////////////////////////
+var number1 = 2,number2 = 5;
+var sumatory = number1 + number2;
+console.log('///////// 1-a /////////');
+console.log('Number 1',number1);
+console.log('Number 2',number2);
+console.log('Sumatory',sumatory);
+
+////////////////// 1-b ///////////////////////////////////////
+var string1 = 'QA-', string2 = 'Course';
+var stringlink= string1 + string2;
+console.log('///////// 1-b /////////');
+console.log('String 1',string1);
+console.log('String 2',string2);
+console.log('String Link',stringlink);
+
+////////////////// 1-c ///////////////////////////////////////
+var stringlg1 = 'pepe', stringlg2 = 'pablo';
+var lengthsumatory = stringlg1.length + stringlg2.length ;
+console.log('///////// 1-c /////////');
+console.log('String length 1',stringlg1.length);
+console.log('String length 2',stringlg2.length);
+console.log('Length sumatory',lengthsumatory);
